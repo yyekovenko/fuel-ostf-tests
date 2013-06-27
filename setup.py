48b2b81c65b7fa525eba9235d6f7ef121b1305a4
Dmitry Shulyak <dshulyak@mirantis.com>
2013-06-27 16:37:26 +0300

    packaging for ostf-tests

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+import io
+
+
+def load_requirements(requirements_path):
+    with io.open(requirements_path, 'r') as reqs:
+        return reqs.read()
+
+
+setuptools.setup(
+
+    name='ostf_tests',
+    version='0.1',
+
+    description='cloud computing testing',
+
+    zip_safe = False,
+
+    packages=setuptools.find_packages(),
+
+    include_package_data=True,
+
+    install_requires=load_requirements('pip-requires'),
+
+)
