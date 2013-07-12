e9edc048f654715c2e9f1bf0535695b28d996b13
Tatyana Leontovich <tleontovich@mirantis.com>
2013-07-12 18:48:24 +0300

    Change testools to unittest2

--- fuel_health/tests/smoke/test_create_flavor.py
+++ fuel_health/tests/smoke/test_create_flavor.py
@@ -1,5 +1,6 @@
+from nose.plugins.attrib import attr
+from nose.tools import timed
 
-from fuel_health.test import attr
 from fuel_health.tests.smoke import base
 
 
@@ -12,6 +13,7 @@ class FlavorsAdminTest(base.BaseComputeAdminTest):
     _interface = 'json'
 
     @attr(type=["fuel", "smoke"])
+    @timed(10.9)
     def test_create_flavor(self):
         """Test low requirements flavor can be created."""
         resp, flavor = self.create_flavor(ram=255,
@@ -19,6 +21,9 @@ class FlavorsAdminTest(base.BaseComputeAdminTest):
 
         self.verify_response_status(
             resp.status, appl="Nova")
+        self.verify_response_body(
+            flavor['name'], u'ost1_test-flavor',
+            msg="Flavor name is not the same as requested.")
         self.verify_response_body_value(
             flavor['disk'], 1,
             msg="Disk size is not the same as requested.")
