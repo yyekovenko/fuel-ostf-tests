e2524c7f8d6085cad3b78639582bef3611a2191b
anaboikina <anaboikina@mirantis.com>
2013-07-15 17:28:53 +0300

    Added fix for flavor creation test.

--- fuel_health/tests/smoke/test_create_flavor.py
+++ fuel_health/tests/smoke/test_create_flavor.py
@@ -15,14 +15,10 @@ class FlavorsAdminTest(base.BaseComputeAdminTest):
     def test_create_flavor(self):
         """Test low requirements flavor can be created."""
         resp, flavor = self.create_flavor(ram=255,
-                                          name='ost1_test-flavor-smoke-test',
                                           disk=1)
 
         self.verify_response_status(
             resp.status, appl="Nova")
-        self.verify_response_body_value(
-            flavor['name'], u'ost1_test-flavor-smoke-test',
-            msg="Flavor name is not the same as requested.")
         self.verify_response_body_value(
             flavor['disk'], 1,
             msg="Disk size is not the same as requested.")
